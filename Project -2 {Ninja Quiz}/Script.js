c47fdadbb34fb96ac4328e63aedfb6f403120ed8
Aditya <aditya.pandey.241103@gmail.com>
2022-11-30 22:24:33 +0530

    Scroll to top

--- Project -2 {Ninja Quiz}/Script.js
+++ Project -2 {Ninja Quiz}/Script.js
@@ -1,5 +1,6 @@
 const correctAnswers = ['B', 'B', 'B', 'B'];
 const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -15,5 +16,10 @@ form.addEventListener('submit', e => {
         }
     });
 
-    console.log(score);
+    //Show result on page;
+    scrollTo(0,0)
+    result.querySelector('span').textContent = `${score}%`
+    result.classList.remove('d-none');
 });
+
+//Window Object {Global object}
